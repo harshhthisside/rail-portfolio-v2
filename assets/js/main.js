b21320dfc6162c0df0ade871b53eb94de905b358
harshr848 <harshr848@gmail.com>
2026-07-12 13:33:08 +0530

    Optimize hero image using WebP

--- assets/js/main.js
+++ assets/js/main.js
@@ -38,3 +38,52 @@ filterButtons.forEach(button => {
     });
 
 });
+/* ===========================
+        MOBILE NAVIGATION
+=========================== */
+
+const menuToggle = document.getElementById("menuToggle");
+const navLinks = document.querySelector(".nav-links");
+
+if (menuToggle && navLinks) {
+
+    menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+
+    document.body.classList.toggle("menu-open");
+
+    const icon = menuToggle.querySelector("i");
+
+    if (navLinks.classList.contains("active")) {
+
+        icon.classList.remove("fa-bars");
+        icon.classList.add("fa-xmark");
+
+    } else {
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    }
+
+});
+
+   document.querySelectorAll(".nav-links a").forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+
+        document.body.classList.remove("menu-open");
+
+        const icon = menuToggle.querySelector("i");
+
+        icon.classList.remove("fa-xmark");
+        icon.classList.add("fa-bars");
+
+    });
+
+});
+
+}
